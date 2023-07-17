bc1f6d7f8392d97258bf2cb4a8f928ea14637201
Luis <lutzcc1@gmail.com>
2023-07-17 07:41:48 -0600

    feature: create preprocess flow

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 .DEFAULT_GOAL := default
 #################### PACKAGE ACTIONS ###################
 reinstall_package:
-	@pip uninstall -y taxifare || :
+	@pip uninstall -y sivico || :
 	@pip install -e .
 
-run_data_extraction:
-	python -c 'from sivico.ml_logic.data import get_senator_initiative_data; get_senator_initiative_data()'
+run_tfidf_preprocess:
+	python -c 'from sivico.interface.main_local import preprocess; preprocess()'
 
-run_api:
-	uvicorn sivico.api.fast:app --reload
+# run_data_extraction:
+# 	python -c 'from sivico.ml_logic.data import get_senator_initiative_data; get_senator_initiative_data()'
+
+# run_api:
+# 	uvicorn sivico.api.fast:app --reload
--- sivico/interface/main_local.py
+++ sivico/interface/main_local.py
@@ -1,10 +1,14 @@
 import pandas as pd
+import os
 
 from sivico.senator_matcher.matchers.tfidf_matcher.preprocessing import preprocess_text
 
 def preprocess() -> None:
     print("Preprocessing...")
 
-    df = pd.read_csv('data/senators_data.csv').head(1)
-    df['preprocessed_summary'] = df['initiatives_summary_dummy'].apply(preprocess_text)
-    df.to_csv('data/senators_processed_data.csv', index=False)
+    project_directory = os.getcwd()
+    project_data_path = os.path.join(project_directory, 'data')
+
+    df = pd.read_csv(os.path.join(project_data_path, 'senators_data_summarized_en.csv')).head(1)
+    df['preprocessed_summary'] = df['initiative_summary_en'].apply(preprocess_text)
+    df.to_csv(os.path.join(project_data_path, 'senators_data_summarized_en_preprocessed.csv'), index=False)
