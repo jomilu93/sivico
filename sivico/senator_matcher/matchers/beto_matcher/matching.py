d22cf4d0db84bcf9cffc985e1cd373dbab1ad554
Jose Luna <jmlunamugica@mac.com>
2023-08-03 15:45:43 -0500

    Final changes for frontend.

--- sivico/senator_matcher/matchers/beto_matcher/matching.py
+++ sivico/senator_matcher/matchers/beto_matcher/matching.py
@@ -26,7 +26,7 @@ def get_top_senators(scores, senators_df, N=5):
     # Add similarity score to the dataframe
     top_senators['similarity_score'] = scores[sorted_indices[:N]]
 
-    return top_senators[['senator_id', 'senadores', 'Fraccion', 'Estado', 'correo', 'url_sitio',
+    return top_senators[['senator_id', 'senadores', 'Apellidos', 'Nombre', 'Fraccion', 'Estado', 'correo', 'url_sitio',
                          'telefono', 'attendance_score', 'similarity_score', 'Salud_initiative_list',
                          'Estudios_Legislativos_initiative_list', 'Educacion_initiative_list',
                          'Para_la_Igualdad_de_Genero_initiative_list', 'Defensa_Nacional_initiative_list',
